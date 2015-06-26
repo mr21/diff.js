5fbe25aeb5f7970a260246cac43970b665d81b2a
Thomas Tortorini <thomastortorini@gmail.com>
2015-06-26 20:04:23 +0200

    tests: window.diff() not accept string anymore

--- tests.js
+++ tests.js
@@ -14,28 +14,32 @@ function arrayEquals( a, b ) {
 	return false;
 }
 
+function test( a, b, expected ) {
+	console.log(
+		arrayEquals(
+			diff(
+				a.split( "" ),
+				b.split( "" )
+			),
+			expected
+		)
+	);
+}
+
 console.log( "unit tests begin" );
 
-console.log( arrayEquals( diff(
-	"",
-	""
-), [] ) );
+test( "", "", [
+] );
 
-console.log( arrayEquals( diff(
-	"Kikoo",
-	"Kikoo"
-), [
+test( "Kikoo", "Kikoo", [
 	[  0,  0,  0, "K" ],
 	[  0,  1,  1, "i" ],
 	[  0,  2,  2, "k" ],
 	[  0,  3,  3, "o" ],
 	[  0,  4,  4, "o" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"012 abc 789",
-	"012 defgh 789"
-), [
+test( "012 abc 789", "012 defgh 789", [
 	[  0,  0,  0, "0" ],
 	[  0,  1,  1, "1" ],
 	[  0,  2,  2, "2" ],
@@ -52,12 +56,9 @@ console.log( arrayEquals( diff(
 	[  0,  8, 10, "7" ],
 	[  0,  9, 11, "8" ],
 	[  0, 10, 12, "9" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"012 defgh 789",
-	"012 abc 789"
-), [
+test( "012 defgh 789", "012 abc 789", [
 	[  0,  0,  0, "0" ],
 	[  0,  1,  1, "1" ],
 	[  0,  2,  2, "2" ],
@@ -74,48 +75,35 @@ console.log( arrayEquals( diff(
 	[  0, 10,  8, "7" ],
 	[  0, 11,  9, "8" ],
 	[  0, 12, 10, "9" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"012",
-	"01"
-), [
+test( "012", "01", [
 	[  0, 0,  0, "0" ],
 	[  0, 1,  1, "1" ],
 	[ -1, 2, -1, "2" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"01",
-	"012"
-), [
+test( "01", "012", [
 	[  0,  0, 0, "0" ],
 	[  0,  1, 1, "1" ],
 	[ +1, -1, 2, "2" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"XMJYAUZ",
-	"MAJAWXUA"
-), [
+test( "XMJYAUZ", "MAJAWXUA", [
 	[ -1,  0, -1, "X" ],
 	[  0,  1,  0, "M" ],
 	[  1, -1,  1, "A" ],
 	[  0,  2,  2, "J" ],
 	[ -1,  3, -1, "Y" ],
-
 	[  0,  4,  3, "A" ],
 	[  1, -1,  4, "W" ],
 	[  1, -1,  5, "X" ],
 	[  0,  5,  6, "U" ],
 	[ -1,  6, -1, "Z" ],
 	[  1, -1,  7, "A" ]
-] ) );
+] );
 
-console.log( arrayEquals( diff(
-	"thomas",
-	"misty thomas"
-), [
+test( "thomas", "misty thomas", [
 	[ +1, -1,  0, "m" ],
 	[ +1, -1,  1, "i" ],
 	[ +1, -1,  2, "s" ],
@@ -128,6 +116,6 @@ console.log( arrayEquals( diff(
 	[  0,  3,  9, "m" ],
 	[  0,  4, 10, "a" ],
 	[  0,  5, 11, "s" ]
-] ) );
+] );
 
 console.log( "unit tests end" );
